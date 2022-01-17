e49a46c84d5d9fb99e5127099b46f330e7e1c6b5
kostayne <fuckogg@yandex.ru>
2022-01-17 20:33:01 +0300

    added footer c (phone)

---
+++ components/footer.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import * as RM from 'react-modifier';
+import Link from 'next/link';
+
+interface FooterProps {
+    headMod?: RM.IModifier;
+}
+
+interface LinkModel {
+    name: string;
+    href: string;
+}
+
+const Footer= (props: FooterProps) => {
+    const headMod = props.headMod || RM.createMod();
+
+    const links:LinkModel[]  = [
+        {
+            name: 'пользовательское соглашение',
+            href: '/terms'
+        },
+
+        {
+            name: 'политика обработки данных',
+            href: '/privacy'
+        },
+    ];
+
+    const getLinksToR = () => {
+        return links.map((l, i) => {
+            return (
+                <Link href={l.href} passHref key={i}>
+                    <a className="text-contrast font-robotoCond">{l.name}</a>
+                </Link>
+            );
+        });
+    };
+
+    return (
+        RM.modElement((
+            <div className="border-t-[1px] border-awhite p-5 flex flex-col mt-6 font-robotoCond">
+                {getLinksToR()}
+                <span>kostayne-dev@yandex.ru</span>
+            </div>
+        ), headMod)
+    );
+};
+
+export default Footer;
