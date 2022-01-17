0efb0215067d74b58dc9bc524f17dd68bc5bce19
kostayne <fuckogg@yandex.ru>
2022-01-17 23:12:17 +0300

    added header links (mobile)

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './header.module.scss';
+import { createModuleStylesConverter } from 'get-module-style';
+import Goto from './goto';
+
+interface HeaderLink {
+    name: string;
+    href: string;
+};
 
 const Header = () => {
-    return (
-        <div className="flex items-center px-5 py-4 border-b-[1px] border-awhite">
-            <span className="font-moderan text-2xl text-contrastAlt">K_DevStack</span>
+    const [isOpened, setOpened] = useState(false);
+    const gs = createModuleStylesConverter(styles);
+    let prevBodyOverflowVal = 'unset';
 
-            <img className="ml-auto" src="/gamburger.svg" />
+    const handleToggleOpen = () => {
+        const newOpenedVal = !isOpened;
+
+        if (newOpenedVal) {
+            prevBodyOverflowVal = document.body.style.overflow;
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = prevBodyOverflowVal;
+        }
+
+        setOpened(newOpenedVal);
+    }
+
+    const bannerCn = isOpened? gs('banner _active') : gs('banner');
+
+    const links: HeaderLink[] = [
+        {
+            name: 'Главная',
+            href: '/'
+        },
+
+        {
+            name: 'Библиотеки / фреймворки',
+            href: '/libs'
+        },
+
+        {
+            name: 'Проекты',
+            href: '/projects'
+        },
+
+        // TODO if uset is not logon, display login here
+
+        {
+            name: 'Профиль',
+            href: '/profile'
+        },
+    ];
+
+    const getMobileLinksToR = () => {
+        return links.map((l, i) => {
+            return (
+                <Goto title={l.name} href={l.href} key={i} />
+            );
+        });
+    };
+
+    return (
+        <div className='bg-[white] z-[2]'>
+            <div className="sticky top-0 flex items-center px-5 py-4 border-b-[1px] border-awhite z-[2]">
+                <span className="font-moderan text-2xl text-contrastAlt">K_DevStack</span>
+                <img onClick={handleToggleOpen} className="ml-auto" src="/gamburger.svg" />
+            </div>
+
+            <div className={bannerCn}>
+                <div className="flex flex-col gap-5 mx-auto mt-6 w-fit">
+                    {getMobileLinksToR()}
+                </div>
+            </div>
         </div>
     );
 };
