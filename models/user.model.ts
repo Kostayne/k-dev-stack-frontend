502cd12d8540be27419caa13cb0bbd4aa061ce08
kostayne <fuckogg@yandex.ru>
2022-01-19 20:33:16 +0300

    added user model

---
+++ models/user.model.ts
@@ -0,0 +1,15 @@
+export interface UserModel {
+    id: number;
+    firstName: string;
+    lastName: string;
+    avatarName: string;
+    email: string;
+    accessLevel: number;
+};
+
+export interface UserCommentRefModel {
+    id: number;
+    firstName: string;
+    lastName: string;
+    avatarName: string;
+};
