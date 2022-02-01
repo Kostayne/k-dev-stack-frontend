9800df65694df8b89de08dc40cf5f6aaa1080694
kostayne <fuckogg@yandex.ru>
2022-02-02 01:08:31 +0300

    added label to custom input c

--- components/styled-text-input.tsx
+++ components/styled-text-input.tsx
@@ -3,6 +3,7 @@ import * as RM from 'react-modifier';
 
 interface StyledTextInputProps {
     value: string;
+    label?: string;
     onChange: (val: string) => void;
     headMod?: RM.IModifier;
 
@@ -19,10 +20,20 @@ const StyledTextInput = (props: StyledTextInputProps) => {
 
     return (
         RM.modElement((
-            <input className={['border-inputInactive border-[2px] rounded-md min-h-[25px]', 
-            'focus:border-inputFocused transition-colors duration-300 outline-none',
-            'px-1 py-[7px]'].join(' ')} 
-            value={props.value} onChange={handleChange} onBlur={props.onBlur} onFocus={props.onFocus} />
+            <div className=''>
+                {/* label */}
+                {props.label && (
+                    <span className='mb-[3px] text-contrast text-sm block'>
+                        {props.label}
+                    </span>
+                )}
+
+                <input className={['border-inputInactive border-[2px] rounded-md min-h-[25px]', 
+                'focus:border-inputFocused transition-colors duration-300 outline-none',
+                'px-1 py-[7px]'].join(' ')} 
+                value={props.value} onChange={handleChange} 
+                onBlur={props.onBlur} onFocus={props.onFocus} />
+            </div>
         ), headMod)
     );
 };
