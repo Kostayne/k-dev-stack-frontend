49b79c100a8d0a16959aa7c7ec42d8abd135ea7c
kostayne <fuckogg@yandex.ru>
2022-02-02 15:05:40 +0300

    added onRouteChanged event

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,23 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from '../components/header';
 import Footer from '../components/footer';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+
+    const onRouteChanged = () => {
+        document.body.style.overflow = 'auto';
+    };
+
+    useEffect(() => {
+        router.events.on('routeChangeComplete', onRouteChanged);
+
+        return () => {
+            router.events.off('routeChangeComplete', onRouteChanged);
+        };
+    });
 
     return (
         <>
