0d78bbd11541d2bc98579e1b1a178a402ae18e9b
kostayne <fuckogg@yandex.ru>
2022-02-05 14:40:29 +0300

    added removable_input c

---
+++ components/removable_input.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import * as RM from 'react-modifier';
+import { useSyntheticInput } from '../hooks/input_synthetic';
+import RmBtnRounded from './rm_btn_rounded';
+import StyledTextInput from './styled-text-input';
+
+interface RemovableInputProps {
+    headMod?: RM.IModifier;
+    initialVal?: string;
+
+    onClick?: () => void;
+    onChnage?: (val: string) => void;
+}
+
+const RemovableInput= (props: RemovableInputProps) => {
+    const input = useSyntheticInput();
+    const headMod = props.headMod || RM.createMod(props.initialVal);
+
+    const onChange = (val: string) => {
+        if (props.onChnage) {
+            props.onChnage(val);
+        }
+
+        input.binding.onChange(val);
+    };
+
+    return (
+        RM.modElement((
+            <div className={['flex items-center'].join(' ')}>
+                <StyledTextInput value={input.binding.value} 
+                onChange={onChange} headMod={RM.createMod(`min-w-[43px]`)} 
+                inputMod={RM.createMod('w-full')} />
+
+                <RmBtnRounded onClick={props.onClick} 
+                headMod={RM.createMod('ml-2')} />
+            </div>
+        ), headMod)
+    );
+};
+
+export default RemovableInput;
