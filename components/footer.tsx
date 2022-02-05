9fecfd91a7d093ba95abe6ba9debd3179ea35899
kostayne <fuckogg@yandex.ru>
2022-02-05 10:39:59 +0300

    improved footer desktop v

--- components/footer.tsx
+++ components/footer.tsx
@@ -38,10 +38,14 @@ const Footer= (props: FooterProps) => {
 
     return (
         RM.modElement((
-            <div className={["border-t-[1px] border-awhite p-5 flex flex-col mt-6 font-robotoCond",
-                            "max-w-[1200px] mx-auto w-full xl:px-0"].join(' ')}>
-                {getLinksToR()}
-                <span>kostayne-dev@yandex.ru</span>
+            // whole footer
+            <div className='border-t-[1px] border-awhite p-5 mt-6 font-robotoCond'>
+                {/* limited content */}
+                <div className={["flex flex-col max-w-[1200px] mx-auto w-full",
+                ""].join(' ')}>
+                    {getLinksToR()}
+                    <span>kostayne-dev@yandex.ru</span>
+                </div>
             </div>
         ), headMod)
     );
