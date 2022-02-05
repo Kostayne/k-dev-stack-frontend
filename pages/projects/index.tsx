6722215333141713b2332a6cf851ed7096d77d78
kostayne <fuckogg@yandex.ru>
2022-02-05 15:37:53 +0300

    added projects p

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,49 +1,68 @@
-import type { NextPage } from 'next';
+import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import * as RM from 'react-modifier';
 import Head from 'next/head';
-import Goto from '../components/goto';
-import TaggedItemPreview from '../components/tagged-item-preview';
+import Goto from '../../components/goto';
+import TaggedItemPreview from '../../components/tagged-item-preview';
+import StyledTextInput from '../../components/styled-text-input';
+import { useSyntheticInput } from '../../hooks/input_synthetic';
 
+interface ProjectsPageProps {
 
-const Home: NextPage = () => {
+}
+
+const Projects: NextPage<ProjectsPageProps> = (props) => {
+	const nameInp = useSyntheticInput();
+	const libsInp = useSyntheticInput();
+
 	return (
 		<div className='page-content'>
 			<Head>
-				<title>K_DevStack</title>
-				<meta name="description" content="Библиотеки, фреймворки, cli утилиты для создания приложений и сайтов, также как и их альтернативы." />
+				<title>Проекты</title>
+				<meta name="description" content="Список проектов с фильтром по имени и используемым библиотекам." />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
 			<main>
-				<h1>Главная</h1>
-				
-				<p className="mt-3">
-					На этом сайте можно найти библиотеки, фреймворки, cli утилиты для создания приложений и сайтов, также как и их альтернативы.
-					<br /> <br />
-					Под каждой библиотекой отображаются мнения пользователей насчет нее и пример кода. Для каждого проекта есть ссылка на исходники, используемый в нем стек.
-				</p>
+				<Goto href='/' title='Проекты' isMainHeading={true} headMod={RM.createMod('mt-5')} 
+				goBack />
 
-				{/* Libs section */}
-				<Goto href='/libs' title='Библиотеки / фреймворки' headMod={RM.createMod('mt-5')} />
+				{/* filter */}
+				<div className='mt-6'>
+					<div className='blue-splitter' />
 
-				<div className="mt-5 previews-list">
-					<TaggedItemPreview name="K React CM" 
-					description="Простая cli утилита для создания компонентов с помощью своих шаблонов. Поддерживает файлы с любыми расширениями, есть возможность автоматического создания стилей основанных на и..." 
-					tags={['утилита', 'react']} href="/libs/react" />
+					{/* centered content */}
+					<div className={['w-fit mx-auto mt-5 flex flex-col gap-4',
+					'md:flex-row md:mx-0 md:items-end'].join(' ')}>
+						{/* inputs */}
+						<div className={['flex flex-col gap-2',
+						'md:flex-row md:gap-3'].join(' ')}>
+							<StyledTextInput {...nameInp.binding} label='Имя' 
+							placeholder='react' />
 
-					<TaggedItemPreview name="K React CM" 
-					description="Простая cli утилита для создания компонентов с помощью своих шаблонов. Поддерживает файлы с любыми расширениями, есть возможность автоматического создания стилей основанных на и..." 
-					tags={['утилита', 'react']} href="/libs/react" />
+							<StyledTextInput {...libsInp.binding} label='Библиотеки' 
+							placeholder='tag1, tag2' />
+						</div>
 
-					<TaggedItemPreview name="K React CM" 
-					description="Простая cli утилита для создания компонентов с помощью своих шаблонов. Поддерживает файлы с любыми расширениями, есть возможность автоматического создания стилей основанных на и..." 
-					tags={['утилита', 'react']} href="/libs/react" />
+						<button className={['primary-btn w-[110px] mx-auto',
+						'md:mb-[2px]'].join(' ')}>применить</button>
+					</div>
+
+					<div className='blue-splitter mt-5' />
 				</div>
 
-				{/* Projects section */}
-				<Goto href='/projects' title='Проекты' headMod={RM.createMod('mt-7')} />
+				<div className='mt-8 previews-list'>
+					<TaggedItemPreview name="K React CM" 
+					description="Простая cli утилита для создания компонентов с помощью своих шаблонов. Поддерживает файлы с любыми расширениями, есть возможность автоматического создания стилей основанных на и..." 
+					tags={['утилита', 'react']} href="/projects/test" />
 
-				<div className="previews-list mt-5">
+					<TaggedItemPreview name="K React CM" 
+					description="Простая cli утилита для создания компонентов с помощью своих шаблонов. Поддерживает файлы с любыми расширениями, есть возможность автоматического создания стилей основанных на и..." 
+					tags={['утилита', 'react']} href="/projects/test" />
+
+					<TaggedItemPreview name="K React CM" 
+					description="Простая cli утилита для создания компонентов с помощью своих шаблонов. Поддерживает файлы с любыми расширениями, есть возможность автоматического создания стилей основанных на и..." 
+					tags={['утилита', 'react']} href="/projects/test" />
+
 					<TaggedItemPreview name="K React CM" 
 					description="Простая cli утилита для создания компонентов с помощью своих шаблонов. Поддерживает файлы с любыми расширениями, есть возможность автоматического создания стилей основанных на и..." 
 					tags={['утилита', 'react']} href="/projects/test" />
@@ -61,4 +80,26 @@ const Home: NextPage = () => {
 	);
 };
 
-export default Home;
+export default Projects;
+
+// export const getStaticProps: GetStaticProps<ProjectsPageProps> = () => {
+// 	return {
+// 		props: {
+
+// 		}
+// 	};
+// };
+
+// export const getStaticPaths: GetStaticPaths = () => {
+// 	return {
+// 		paths: [
+// 			{
+// 				params: {
+
+// 				}
+// 			}
+// 		],
+
+// 		fallback: 'blocking'
+// 	};
+// };
