2df50a77943a868d7594921542f7795837cf5395
kostayne <fuckogg@yandex.ru>
2022-02-18 01:50:17 +0300

    added header_mobile_links_list

---
+++ components/header_mobile_links_list.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import * as RM from 'react-modifier';
+import Goto from './goto';
+import { HeaderLink } from './header';
+
+interface HeaderMobileLinksProps {
+    headMod?: RM.IModifier;
+    links: HeaderLink[];
+    disableTabNavigation: boolean;
+    onLinkClick: () => void;
+}
+
+const HeaderMobileLinksList= (props: HeaderMobileLinksProps) => {
+    const headMod = props.headMod || RM.createMod();
+    const { disableTabNavigation, onLinkClick } = props;
+
+    const getMobileLinksToR = () => {
+        return props.links.map((l, i) => {
+            return (
+                <Goto title={l.name} href={l.href} key={l.name} 
+                onLinkClick={onLinkClick} disableTabNavigation={disableTabNavigation} />
+            );
+        });
+    };
+
+    return (
+        RM.modElement((
+            <div className="flex flex-col gap-0 max-w-[1200px]">
+                {getMobileLinksToR()}
+            </div>
+        ), headMod)
+    );
+};
+
+export default HeaderMobileLinksList;
