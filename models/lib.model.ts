2facbca4a62da8944f2d7c97cc4db2b201f26f6e
kostayne <fuckogg@yandex.ru>
2022-02-23 13:39:22 +0300

    added lib named link model

--- models/lib.model.ts
+++ models/lib.model.ts
@@ -14,3 +14,8 @@ export interface LibModel {
     alternatives: NamedLink[];
     comments: CommentModel[];
 }
+
+export interface LibNamedLinkModel {
+    libId: number;
+    namedLinkId: number;
+}
