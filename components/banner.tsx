a13dc31fbb10460f7d84de3a4c7afd2d53f76651
kostayne <fuckogg@yandex.ru>
2022-03-11 08:50:05 +0300

    added banner c

---
+++ components/banner.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as RM from 'react-modifier';
+
+interface BannerProps {
+    headMod?: RM.IModifier;
+}
+
+const Banner= (props: BannerProps) => {
+    const headMod = props.headMod || RM.createMod();
+
+    return (
+        RM.modElement((
+            <div className='fixed left-0 right-0 top-0 bottom-0 bg-[white]'>
+                
+            </div>
+        ), headMod)
+    );
+};
+
+export default Banner;
