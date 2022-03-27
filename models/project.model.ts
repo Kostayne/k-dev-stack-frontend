240d46e486955be6969e407eb691fb2e2cd98489
kostayne <fuckogg@yandex.ru>
2022-03-27 21:21:36 +0300

    refactored projModel

--- models/project.model.ts
+++ models/project.model.ts
@@ -1,14 +1,15 @@
-import { CommentModel, CommentPersonalizedModel } from "./comment.model";
+import { CommentPersonalizedModel } from "./comment.model";
 import { LibModel } from "./lib.model";
 import { NamedLinkModel } from "./named_link.model";
 
 export interface ProjectModel {
     id: number;
+    slug: string;
     name: string;
+    tags: string[];
     description: string;
     libs: LibModel[];
     sources: NamedLinkModel[];
     comments: CommentPersonalizedModel[];
-    slug: string;
-    tags: string[];
+    // TODO remove commets, since they loading async
 };
