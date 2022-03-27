c5e3c334a4c90a9e2cece9f3183dc538e00602e2
kostayne <fuckogg@yandex.ru>
2022-03-27 21:22:01 +0300

    added comment_loader c

---
+++ components/comment_loader.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+import * as RM from 'react-modifier';
+import 'react-loading-skeleton/dist/skeleton.css'
+
+interface CommentLoaderProps {
+    headMod?: RM.IModifier;
+}
+
+const CommentLoader= (props: CommentLoaderProps) => {
+    const headMod = props.headMod || RM.createMod();
+
+    return (
+        RM.modElement((
+            <div className='flex w-[350px] h-[150px]'>
+                <Skeleton circle width={50} height={50} />
+
+                {/* right part */}
+                <div className='ml-3'>
+                    <Skeleton height={15} width={90} />
+                    <Skeleton height={15} width={350} style={{
+                        marginTop: 10
+                    }} />
+                </div>
+            </div>
+        ), headMod)
+    );
+};
+
+export default CommentLoader;
