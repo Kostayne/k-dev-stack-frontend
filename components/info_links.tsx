6004066594ad664bac9f1f23000d6e55b0c39ae0
kostayne <fuckogg@yandex.ru>
2022-04-18 21:09:36 +0300

    added info_links c

---
+++ components/info_links.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import * as RM from 'react-modifier';
+import { NamedLinkModel } from '../models/named_link.model';
+
+interface InfoLinksProps {
+    headMod?: RM.IModifier;
+    links: NamedLinkModel[];
+}
+
+const InfoLinks= (props: InfoLinksProps) => {
+    const headMod = props.headMod || RM.createMod();
+
+    const getLinksListToR = () => {
+        return props.links.map((l, i) => {
+            const _name = i < props.links.length - 1? l.name + ',' : l.name;
+
+            return (
+                <a className='text-link underline' href={l.href} key={i}>{_name}</a>
+            );
+        });
+    };
+
+    return (
+        RM.modElement((
+            <div>
+                {/* field name */}
+                <span className='text-info-field'>Ссылки</span>
+
+                {/* links list */}
+                <div className='flex gap-[5px] text-[18px] mt-[5px]'>
+                    {getLinksListToR()}
+                </div>
+            </div>
+        ), headMod)
+    );
+};
+
+export default InfoLinks;
