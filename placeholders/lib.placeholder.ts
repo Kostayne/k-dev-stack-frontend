e32a7b4b1f5bf084b3bf0b323ad5b8bcc793fdc9
kostayne <fuckogg@yandex.ru>
2022-04-18 22:12:27 +0300

    added lib placeholder

---
+++ placeholders/lib.placeholder.ts
@@ -0,0 +1,13 @@
+import { NamedLinkModel } from "../models/named_link.model";
+
+export const libInfoLinksPlaceholder: NamedLinkModel[] = [
+    {
+        name: 'npm',
+        href: 'https://npmjs.com'
+    },
+
+    {
+        name: 'github',
+        href: 'https://github.com'
+    }
+];
