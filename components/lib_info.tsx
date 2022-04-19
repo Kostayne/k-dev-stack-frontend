0a789f8f02abe57b91fdb807cd805b6e03fcd953
kostayne <fuckogg@yandex.ru>
2022-04-19 11:57:23 +0300

    refactored lib_info fields

--- components/lib_info.tsx
+++ components/lib_info.tsx
@@ -4,13 +4,14 @@ import { ToolType } from '../enums/tool_type.enum';
 import { NamedLinkModel } from '../models/named_link.model';
 import { getToolTypeName } from '../utils/get_tool_type_name';
 import InfoLinks from './info_links';
+import LowContrastInfoField from './low_contrast_info_field';
 
 interface LibInfoProps {
     headMod?: RM.IModifier;
     links: NamedLinkModel[];
     toolType: ToolType;
     issuesCount: number;
-    licence: string;
+    license: string;
     downloads: string;
     lastUpdate: string;
     weight: string;
@@ -31,47 +32,23 @@ const LibInfo= (props: LibInfoProps) => {
 
                 {/* second row */}
                 <div className='flex gap-x-[15px]'>
-                    <div className='flex flex-col'>
-                        <span className='tool-info-field block'>Вид</span>
-                        <span className='mt-[auto]'>{getToolTypeName(props.toolType)}</span>
-                    </div>
-
-                    <div className=''>
-                        <span className='tool-info-field'>Issues</span>
-                        <span className='mt-[5px] block'>{props.issuesCount}</span>
-                    </div>
+                    <LowContrastInfoField name='Вид' value={getToolTypeName(props.toolType)} />
+                    <LowContrastInfoField name='Issues' value={props.issuesCount} />
                 </div>
 
                 {/* third row */}
                 <div className='flex gap-x-[15px]'>
-                    <div className=''>
-                        <span className='tool-info-field'>Лицензия</span>
-                        <span className='mt-[5px] block'>{props.licence}</span>
-                    </div>
-
-                    <div className=''>
-                        <span className='tool-info-field'>Скачиваний</span>
-                        <span className='mt-[5px] block'>{props.downloads}</span>
-                    </div>
+                    <LowContrastInfoField name='Лицензия' value={props.license} />
+                    <LowContrastInfoField name='Скачиваний' value={props.downloads} />
                 </div>
 
                 {/* fourth row */}
-                <div className=''>
-                    <span className='tool-info-field'>Последнее обновление</span>
-                    <span className='mt-[5px] block'>{props.lastUpdate}</span>
-                </div>
+                <LowContrastInfoField name='Последнее обновление' value={props.lastUpdate} />
 
                 {/* fiveth row */}
                 <div className='flex gap-x-[15px]'>
-                    <div>
-                        <span className='tool-info-field'>Вес</span>
-                        <span className='mt-[5px] block'>{props.weight}</span>
-                    </div>
-
-                    <div>
-                        <span className='tool-info-field'>Версия</span>
-                        <span className='mt-[5px] block'>{props.version}</span>
-                    </div>
+                    <LowContrastInfoField name='Вес' value={props.weight} />
+                    <LowContrastInfoField name='Версия' value={props.version} />
                 </div>
             </div>
         ), headMod)
