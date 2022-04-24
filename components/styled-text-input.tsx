a20ec15bd338d23049ef14ddb75b6f3e962fae63
kostayne <fuckogg@yandex.ru>
2022-04-24 12:03:06 +0300

    added autocomplete feature to styled_text_input c

--- components/styled-text-input.tsx
+++ components/styled-text-input.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as RM from 'react-modifier';
+import { useStyledTextInput } from '../hooks/styled_text_input.hook';
+import AutocompleteOptionsList from './autocomplete-options-list';
 
-interface StyledTextInputProps {
+export interface StyledTextInputProps {
     value: string;
     label?: string;
     name?: string;
     placeholder?: string;
     type?: string;
+    autocompleteOptions?: string[];
 
     onChange: (val: string) => void;
     onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
@@ -14,7 +17,6 @@ interface StyledTextInputProps {
 
     headMod?: RM.IModifier;
     inputMod?: RM.IModifier;
-
     style?: React.CSSProperties;
 }
 
@@ -22,26 +24,45 @@ const StyledTextInput = (props: StyledTextInputProps) => {
     const headMod = props.headMod || RM.createMod();
     const inputMod = props.inputMod || RM.createMod();
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        props.onChange(e.currentTarget.value);
-    };
+    const {
+        handleChange,
+        handleSelectedOption,
+        onBlur,
+        onFocus,
+        onKeyDown,
+        showAutoComplete,
+        autocompleteOptions,
+        selectedOption,
+        inputWrapperRef,
+        inputRef
+    } = useStyledTextInput(props);
 
     return (
         RM.modElement((
-            <div className='' style={props.style}>
+            <div className='' style={props.style} ref={inputWrapperRef}>
                 {props.label && (
                     <span className='mb-[3px] text-contrast text-sm block'>
                         {props.label}
                     </span>
                 )}
 
-                {RM.modElement((
-                    <input className={['border-inputInactive border-[2px] rounded-md min-h-[25px]', 
-                    'focus:border-inputFocused transition-colors duration-300 outline-none',
-                    'px-1 py-[7px]'].join(' ')} 
-                    value={props.value} onChange={handleChange} placeholder={props.placeholder}
-                    onBlur={props.onBlur} onFocus={props.onFocus} type={props.type} name={props.name} />
-                ), inputMod)}
+                <div className='relative'>
+                    {RM.modElement((
+                        <input className={['border-inputInactive border-[2px] rounded-md min-h-[25px]', 
+                        'focus:border-inputFocused transition-colors duration-300 outline-none',
+                        'px-1 py-[7px] z-[1]'].join(' ')} 
+                        value={props.value} onChange={handleChange} placeholder={props.placeholder}
+                        onBlur={onBlur} onFocus={onFocus} type={props.type} name={props.name}
+                        onKeyDown={onKeyDown} ref={inputRef} />
+                    ), inputMod)}
+
+                    {autocompleteOptions.length > 0 && showAutoComplete && (
+                        <AutocompleteOptionsList options={autocompleteOptions}
+                        onSelectedOption={handleSelectedOption}
+                        selectedOption={selectedOption} 
+                        headMod={RM.createMod('w-full pt-[7px] shadow-baseShadow translate-y-[-3px]')} />
+                    )}
+                </div>
             </div>
         ), headMod)
     );
---
+++ hooks/styled_text_input.hook.ts
@@ -0,0 +1,134 @@
+import { useEffect, useRef, useState } from "react";
+import { StyledTextInputProps } from "../components/styled-text-input";
+
+export function useStyledTextInput(props: StyledTextInputProps) {
+    const allAutocompleteOptions = props.autocompleteOptions || [];
+    const [showAutoComplete, setShowAutoComplete] = useState(false);
+    const [autocompleteOptions, setAutocompleteOptions] = useState<string[]>([]);
+    const [selectedOption, setSelectedOption] = useState(allAutocompleteOptions[0] || '');
+    const inputWrapperRef = useRef<HTMLInputElement>(null);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        // don't create listeners when there is no
+        // autocomplete options
+        if (!props.autocompleteOptions) {
+            return;
+        }
+
+        if (props.autocompleteOptions.length == 0) {
+            return;
+        }
+
+        // hide autocomplete when change focus
+        window.addEventListener('focus', (e) => {
+            if (e.target == inputRef.current) {
+                return;
+            }
+
+            setShowAutoComplete(false);
+        });
+
+        // hide autocomplete when click outside
+        window.addEventListener('click', (e) => {
+            const tg = e.target as HTMLElement;
+
+            if (!tg) {
+                return;
+            }
+
+            // clicked on component
+            if (inputWrapperRef.current?.contains(tg) || tg == inputWrapperRef.current) {
+                return;
+            }
+
+            setShowAutoComplete(false);
+        });
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newVal = e.currentTarget.value;
+        let newOptions = allAutocompleteOptions;
+
+        // fixes autocomplete after option select
+        setShowAutoComplete(true);
+
+        if (newVal != '') {
+            const filteredOptions = allAutocompleteOptions.filter(o => {
+                return o.includes(newVal);
+            });
+
+            newOptions = filteredOptions;
+        }
+
+        setAutocompleteOptions(newOptions);
+        props.onChange(newVal);
+    };
+
+    const handleSelectedOption = (v: string) => {
+        props.onChange(v);
+        setShowAutoComplete(false);
+    };
+
+    const onFocus = () => {
+        setShowAutoComplete(true);
+        setAutocompleteOptions(allAutocompleteOptions);
+        props.onFocus?.call(null);
+    };
+
+    const onBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+        props.onBlur?.call(null, e);
+    };
+
+    const onKeyDown = (e: React.KeyboardEvent) => {
+        // no need to process this event
+        // if no autocomplete options
+        if (autocompleteOptions.length == 0) {
+            return;
+        }
+
+        // process enter key
+        if (e.key == 'Enter') {
+            props.onChange(selectedOption);
+            setShowAutoComplete(false);
+            return;
+        }
+
+        // process arrows keys
+        const curIndex = autocompleteOptions.findIndex((v) => v == selectedOption);
+        let wantedIndex = curIndex;
+
+        if (e.key == 'ArrowUp') {
+            wantedIndex -= 1;
+        }
+
+        if (e.key == 'ArrowDown') {
+            wantedIndex += 1;
+        }
+
+        if (wantedIndex >= autocompleteOptions.length) {
+            setSelectedOption(autocompleteOptions[0]);
+            return;
+        }
+
+        if (wantedIndex < 0) {
+            setSelectedOption(autocompleteOptions.at(-1) as string);
+            return;
+        }
+
+        setSelectedOption(autocompleteOptions[wantedIndex]);
+    };
+
+    return {
+        onFocus,
+        onKeyDown,
+        onBlur,
+        handleSelectedOption,
+        handleChange,
+        showAutoComplete,
+        autocompleteOptions,
+        selectedOption,
+        inputWrapperRef,
+        inputRef
+    };
+}
