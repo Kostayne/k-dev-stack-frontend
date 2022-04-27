d8dd3e13fd5c8207795ec51bc7f3f9de9e7f528b
kostayne <fuckogg@yandex.ru>
2022-04-27 10:21:34 +0300

    rewrote create_btn_rounded styles to tailwind

--- components/create_btn_rounded.tsx
+++ components/create_btn_rounded.tsx
@@ -14,9 +14,16 @@ const CreateBtnRounded= (props: CreateBtnRoundedProps) => {
 
     return (
         RM.modElement((
-            <button className={gs('btn')} onClick={props.onClick}>
+            <button className={[
+                'w-[23px] h-[23px] rounded-[50%] border-none',
+                'bg-btnInactive hover:bg-btnActive',
+                'cursor-pointer flex items-center justify-center',
+                'transition-[300ms] ease-in'
+            ].join(' ')} onClick={props.onClick}>
                 {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img alt="create" src="/icons/plus.svg" className={gs('img')} />
+                <img alt="create" src="/icons/plus.svg" className={[
+                    'ml-[1px] object-cover'
+                ].join(' ')} />
             </button>
         ), headMod)
     );
