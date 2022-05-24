9d84f1b050ce51a548983ce691c75d4922f1a157
kostayne <fuckogg@yandex.ru>
2022-05-24 10:37:35 +0300

    added tag_rounded_list wrap prop

--- components/tag_rounded_list.tsx
+++ components/tag_rounded_list.tsx
@@ -26,7 +26,7 @@ const TagRoundedList= (props: TagRoundedListProps) => {
 
     return (
         RM.modElement((
-            <div className='flex gap-2'>
+            <div className='flex flex-wrap gap-2'>
                 {getItemsToR()}
             </div>
         ), headMod)
